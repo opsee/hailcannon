99ebd5d9ad4e24c1c4eeebe048bb2d626da629d1
Dan Compton <dan@opsee.com>
2016-04-29 16:44:40 -0400

    Fix run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,4 @@ APPENV=${APPENV:-hailcannonenv}
 /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
 
 source /$APPENV && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/vape.key > /vape.key && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$HAILCANNON_CERT > /$HAILCANNON_CERT && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$HAILCANNON_CERT_KEY > /$HAILCANNON_CERT_KEY && \
-  chmod 600 /$HAILCANNON_CERT_KEY && \
 	/hailcannon
